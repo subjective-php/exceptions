a15d4bad71616902e205c0d52a7847bfd890e929
chadicus <chadwickgray@gmail.com>
2014-04-17 17:38:17 -0400

    Add two exceptions

---
+++ src/Chadicus/Exception/BadPropertyException.php
@@ -0,0 +1,7 @@
+<?php
+namespace Chadicus\Exception;
+
+class BadPropertyException extends \Exception
+{
+
+}
---
+++ src/Chadicus/Exception/NotFoundException.php
@@ -0,0 +1,7 @@
+<?php
+namespace Chadicus\Exception;
+
+class NotFoundException extends \Exception
+{
+
+}
