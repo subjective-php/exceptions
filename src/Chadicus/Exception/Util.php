718d7ca54877dab69c147062c4d3df3b303312ea
chadicus <chadwickgray@gmail.com>
2014-05-09 12:11:12 -0400

    Add Chadicus\Exceptions\Util::raise()

--- src/Chadicus/Exception/Util.php
+++ src/Chadicus/Exception/Util.php
@@ -2,14 +2,14 @@
 namespace Chadicus\Exception;
 
 /**
- * Static utility class for exceptions.
+ * Static utility class for exceptions
  */
 class Util
 {
     /**
      * Returns the Exception that is the root cause of one or more subsequent exceptions.
      *
-     * @return \Exception
+     * @return Exception
      */
     final public static function getBaseException(\Exception $exception)
     {
@@ -18,5 +18,22 @@ class Util
         }
 
         return $exception;
+    }
+
+    /**
+     * Throws a new \ErrorException based on the error information provided.
+     * To be used as a callback for {@see set_error_handler()}
+     *
+     * @return bool false
+     *
+     * @throws \ErrorException
+     */
+    final public static function raise($level, $message, $file = null, $line = null)
+    {
+        if (error_reporting() === 0) {
+            return false;
+        }
+
+        throw new \ErrorException($message, 0, $level, $file, $line);
     }
 }
--- tests/Chadicus/Exception/UtilTest.php
+++ tests/Chadicus/Exception/UtilTest.php
@@ -38,4 +38,43 @@ final class UtilTest extends \PHPUnit_Framework_TestCase
         $e = new \Exception();
         $this->assertSame($e, Util::getBaseException($e));
     }
+
+    /**
+     * Verifies basic behavior of raise().
+     *
+     * @test
+     * @covers ::raise
+     *
+     * @return void
+     */
+    public function raise()
+    {
+        set_error_handler('\Chadicus\Exception\Util::raise');
+        try {
+            trigger_error('test', E_USER_NOTICE);
+        } catch (\ErrorException $e) {
+            $this->assertSame('test', $e->getMessage());
+            $this->assertSame(0, $e->getCode());
+            $this->assertSame(E_USER_NOTICE, $e->getSeverity());
+            $this->assertSame((__LINE__) - 5, $e->getLine());
+            $this->assertSame(__FILE__, $e->getFile());
+        }
+
+        restore_error_handler();
+    }
+
+    /**
+     * Verifies raise() returns false when error reporting is disabled.
+     *
+     * @test
+     * @covers ::raise
+     *
+     * @return void
+     */
+    public function raiseErrorReportingDisabled()
+    {
+        $restoreLevel = error_reporting(0);
+        $this->assertFalse(Util::raise(E_USER_NOTICE, 'test', __FILE__, __LINE__));
+        error_reporting($restoreLevel);
+    }
 }
