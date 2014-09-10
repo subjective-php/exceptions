b75e65d9a4d0336e9ae34c6b6d613f5294d057c8
Chad Gray <chadwickgray@gmail.com>
2014-09-10 15:52:54 -0400

    Add FormatException

---
+++ src/FormatException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Chadicus;
+
+/**
+ * Exception thrown when the format of an argument is invalid, or when a format string is not well formed.
+ */
+class FormatException extends \InvalidArgumentException
+{
+}
